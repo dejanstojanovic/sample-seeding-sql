687f5e107f075f1c760a3dbb41623138a54e4150
Dejan Stojanovic <d.stojanovic@hotmail.com>
2020-09-12 10:53:34 +0400

    Feature/seeding tool (#1)
    * feat: adds global tool project
    
    * feat: adds embeded resource SQL script template
    
    * feat: writing output script file
    
    * feat: adds message after script file created
    
    * feat: adds project update and save
    
    * chore: project clean up
    
    * feat: updates help command
    
    * feat: updates package config
    
    * feat: adds cli tool packege config
    
    * feat: updates project info
    
    Co-authored-by: Dejan Stojanovic <dejan@evowition.com>

---
+++ Sample.Seeding.Tool/Template.sql
@@ -0,0 +1,5 @@
+﻿/******** Add you seeeding script here ********/
+
+
+/******* Comment out or remove following line once you add your script ********/
+RAISERROR ('No seeding script implemented', 16,1)

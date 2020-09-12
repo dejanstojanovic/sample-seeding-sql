5f11fc4dc655c31fa713e89b4037249cb43b83d9
Dejan Stojanovic <d.stojanovic@hotmail.com>
2020-09-12 10:53:56 +0400

    Revert "Feature/seeding tool (#1)" (#2)
    This reverts commit 687f5e107f075f1c760a3dbb41623138a54e4150.

--- Sample.Seeding.Tool/Template.sql
+++
@@ -1,5 +0,0 @@
-﻿/******** Add you seeeding script here ********/
-
-
-/******* Comment out or remove following line once you add your script ********/
-RAISERROR ('No seeding script implemented', 16,1)

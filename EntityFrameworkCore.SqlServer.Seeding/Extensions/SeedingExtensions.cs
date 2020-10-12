fe8ef53cd451e4ee84c4bcb257d6151ad1b5c5ab
Dejan Stojanovic <dejan@evowition.com>
2020-10-12 21:08:18 +0400

    feat: adds seeding db context

---
+++ EntityFrameworkCore.SqlServer.Seeding/Extensions/SeedingExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore.SqlServer.Seeding.Extensions
+{
+    public static class SeedingExtensions
+    {
+        public static void AddScriptSeeding(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EmployeesDatabaseContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString(DbContextConfigConstants.DB_CONNECTION_CONFIG_NAME),
+                    x =>
+                    {
+                        x.MigrationsHistoryTable("__EFMigrationsHistory");
+                        x.MigrationsAssembly(typeof(void).Assembly.GetName().Name);
+                    }
+                );
+            });
+        }
+    }
+}
